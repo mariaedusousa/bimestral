9a7c17d12743cdaab10c4d75546af9cc437e2150
mariaedusousa <mariaeduardasousasantoss071@gmail.com>
2025-08-14 09:25:09 -0300

    checagem de numero maior.c

---
+++ checagem denumero maior.c
@@ -0,0 +1,37 @@
+include<stdio.h>
+
+int main(){
+
+int quantidade, numero, maior;
+
+int contador=0;
+
+printf("informe a quantidade desejada:");
+
+scanf("%d", &quantidade);
+
+for(int i=0;i<quantidade;i++){
+
+printf("informe o numero%d: ",i+1);
+
+scanf("%d",&numero);
+
+if(i==0||numero maior){
+
+maior numero:
+
+corntador-1:
+
+} else if (numero==maior){
+
+contador++;
+
+}
+
+printf("\n---Resultado---\n");
+
+printf("O maior numero é:%d\n",maior):
+
+printf("este numero foi lido 96d vezes.\n",contador);
+
+return 0;}
