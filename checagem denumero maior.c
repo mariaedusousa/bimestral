8ee75423a9998bafef9c47bbc4120302ff6f914d
mariaedusousa <mariaeduardasousasantoss071@gmail.com>
2025-08-15 07:14:12 -0300

    checagem denumero maior.c

--- checagem denumero maior.c
+++ checagem denumero maior.c
@@ -1,37 +1,37 @@
-include<stdio.h>
 
-int main(){
+#include <stdio.h> 
+int main() {
+    int quantidade;
+    int numero;
+    int maior = 0; 
+    int contador = 0;
 
-int quantidade, numero, maior;
+    printf("Informe a quantidade desejada: ");
+    scanf("%d", &quantidade);
 
-int contador=0;
+    
+    if (quantidade <= 0) {
+        printf("A quantidade deve ser maior que zero.\n");
+        return 1; 
+    }
 
-printf("informe a quantidade desejada:");
+    for (int i = 0; i < quantidade; i++) {
+        printf("Informe o numero %d: ", i + 1);
+        scanf("%d", &numero);
 
-scanf("%d", &quantidade);
+        
+        if (i == 0 || numero > maior) {
+            maior = numero;      
+            contador = 1;      
+        } else if (numero == maior) {
+            contador++;        
+        }
+    }
 
-for(int i=0;i<quantidade;i++){
+    
+    printf("\n--- Resultado ---\n");
+    printf("O maior numero é: %d\n", maior);
+    printf("Este numero foi lido %d vezes.\n", contador);
 
-printf("informe o numero%d: ",i+1);
-
-scanf("%d",&numero);
-
-if(i==0||numero maior){
-
-maior numero:
-
-corntador-1:
-
-} else if (numero==maior){
-
-contador++;
-
+    return 0; 
 }
-
-printf("\n---Resultado---\n");
-
-printf("O maior numero é:%d\n",maior):
-
-printf("este numero foi lido 96d vezes.\n",contador);
-
-return 0;}
